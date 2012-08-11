f7327f93577fb92c4bd7ad05a207bbb6518fca7d
Scott Morrison <scott.morrison@autodesk.com>
2012-08-11 10:26:35 -0700

    Create AutoWord project

---
+++ AutoWord/AwModel.h
@@ -0,0 +1,17 @@
+//
+//  AwModel.h
+//  AutoWord
+//
+//  Created by Scott C Morrison on 8/11/12.
+//  Copyright (c) 2012 Scott C Morrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AwModel : NSObject
+{
+}
+
+@property int number;
+
+@end
---
+++ AutoWord/AwModel.m
@@ -0,0 +1,15 @@
+//
+//  AwModel.m
+//  AutoWord
+//
+//  Created by Scott C Morrison on 8/11/12.
+//  Copyright (c) 2012 Scott C Morrison. All rights reserved.
+//
+
+#import "AwModel.h"
+
+@implementation AwModel
+
+@synthesize number;
+
+@end
